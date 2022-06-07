71b468f412d9535505f45ccae1fbdc25e587d67c
felrobotics <lposadaa@eafit.edu.co>
2022-06-07 20:19:02 +0000

    updated env vars and readme

--- README.md
+++ README.md
@@ -8,15 +8,25 @@ Disclaimer: This demo is only for educational purposes, this setup should not be
 
 ## As user "root"
 
-1. Connect to server with ssh root@host and clone the git repo 
-2. Create user "airflow" by running the script create_users.sh
+1. Connect to server with ssh root@host and clone the repo  `git clone https://github.com/felrobotics/deploy-airflow-celery-ubuntu.git`
+2. Create user "airflow" by running the script setup/create_users.sh
 3. Logout root (not recommened to work as root user)
 
 ## Setup with user "airflow"
 
-1. Login ssh airflow@host and clone the git repo
-2. Install ubuntu packages with install_packages.sh
+1. Login ssh airflow@host and clone the repo `git clone https://github.com/felrobotics/deploy-airflow-celery-ubuntu.git`
+2. Install ubuntu packages with setup/install_packages.sh
    
 ## Upload Secrets   
 
 1. Logout from server and run in your local machine the command: `rsync -e ssh secrets.sh  airflow@host:deploy-airflow-celery-ubuntu/`
+
+## Setup Python & Airflow
+
+1. Setup python and virtual envs setup/running setup_python.sh
+2. Setup airflow database schema with setup/setup_schemas.sh
+3. Setup airflow db and users by running setup/setup_airflow
+
+## Launch Airflow
+
+1. run script launch_airflow.sh
---
+++ env_vars.sh
@@ -0,0 +1,93 @@
+# HELPERS
+# colors
+nc=$(echo -en '\033[0m')
+red=$(echo -en '\033[00;31m')
+green=$(echo -en '\033[00;32m')
+yellow=$(echo -en '\033[00;33m')
+function msg {
+  echo -e "${yellow}$1 ${nc}$2"
+}
+
+
+# LOAD SECRETS 
+source secrets.sh
+
+echo -e "\n\n${green}--------------------------------------"
+echo -e "          AIRFLOW  env vars"
+echo -e "--------------------------------------${nc}"
+
+# AIRFLOW HOME
+AIRFLOW_HOME="${HOME}/airflow"
+export AIRFLOW_HOME
+msg "airflow_home:" $AIRFLOW_HOME
+
+# DAGS FOLDER
+OMAR_HOME="${HOME}/omni/dev/ds-modeling"
+AIRFLOW__CORE__DAGS_FOLDER="${OMAR_HOME}/omar_airflow/airflow-data/dags"
+export AIRFLOW__CORE__DAGS_FOLDER
+msg "dags_folder:" $AIRFLOW__CORE__DAGS_FOLDER
+
+#[core]
+echo -e "\n${green}[core]${nc}"
+
+# EXECUTOR
+# AIRFLOW__CORE__EXECUTOR=LocalExecutor
+AIRFLOW__CORE__EXECUTOR=CeleryExecutor # CELERY
+export AIRFLOW__CORE__EXECUTOR
+msg "executor:" $AIRFLOW__CORE__EXECUTOR
+
+
+AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgresql+psycopg2://${POSTGRES_USERNAME}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}${POSTGRES_EXTRAS}"
+export AIRFLOW__CORE__SQL_ALCHEMY_CONN
+msg "sql_alchemy_conn:" $AIRFLOW__CORE__SQL_ALCHEMY_CONN
+
+
+# sql_alchemy_schema = omni_mlops_local_luisposada_e8d0fcfe0f4d_aif
+AIRFLOW__CORE__SQL_ALCHEMY_SCHEMA=$POSTGRES_SCHEMA
+export AIRFLOW__CORE__SQL_ALCHEMY_SCHEMA
+msg "sql_alchemy_schema" $AIRFLOW__CORE__SQL_ALCHEMY_SCHEMA
+
+
+# plugins_folder = /home/luisposada/airflow/plugins
+AIRFLOW__CORE__PLUGINS_FOLDER="${AIRFLOW_HOME}/plugins"
+export AIRFLOW__CORE__PLUGINS_FOLDER
+msg "plugins_folder" $AIRFLOW__CORE__SQL_ALCHEMY_SCHEMA
+
+#---------------------------------------
+# [logging]
+echo -e "\n${green}[logging]${nc}"
+
+
+# base_log_folder = /home/luisposada/airflow/logs
+AIRFLOW__LOGGING__BASE_LOG_FOLDER="${AIRFLOW_HOME}/logs"
+export AIRFLOW__LOGGING__BASE_LOG_FOLDER
+msg "base_log_folder" $AIRFLOW__LOGGING__BASE_LOG_FOLDER
+
+
+# dag_processor_manager_log_location = /home/luisposada/airflow/logs/dag_processor_manager/dag_processor_manager.log
+AIRFLOW__LOGGING__DAG_PROCESSOR_MANAGER_LOG_LOCATION="${AIRFLOW_HOME}/logs/dag_processor_manager/dag_processor_manager.log"
+export AIRFLOW__LOGGING__DAG_PROCESSOR_MANAGER_LOG_LOCATION
+msg "base_log_folder" $AIRFLOW__LOGGING__DAG_PROCESSOR_MANAGER_LOG_LOCATION
+
+
+# [scheduler]
+echo -e "\n${green}[scheduler]${nc}"
+# child_process_log_directory = /home/luisposada/airflow/logs/scheduler
+AIRFLOW__SCHEDULER__CHILD_PROCESS_LOG_DIRECTORY="${AIRFLOW_HOME}/logs/scheduler"
+export AIRFLOW__SCHEDULER__CHILD_PROCESS_LOG_DIRECTORY
+msg "child_process_log_directory" $AIRFLOW__SCHEDULER__CHILD_PROCESS_LOG_DIRECTORY
+
+#---------------------------------------
+# [celery]
+echo -e "\n${green}[celery]${nc}"
+
+# DEFAULTS
+# broker_url = redis://redis:6379/0
+# result_backend = db+postgresql://postgres:airflow@postgres/airflow
+# NEW VALUES
+AIRFLOW__CELERY__BROKER_URL="redis://${REDIS_PASSWORD}@${REDIS_HOST}:${REDIS_PORT}/0"
+AIRFLOW__CELERY__RESULT_BACKEND="db+postgresql://${POSTGRES_USERNAME}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}${POSTGRES_EXTRAS}"
+export AIRFLOW__CELERY__BROKER_URL
+export AIRFLOW__CELERY__RESULT_BACKEND
+msg "broker_url" $AIRFLOW__CELERY__BROKER_URL
+msg "result_backend" $AIRFLOW__CELERY__RESULT_BACKEND
