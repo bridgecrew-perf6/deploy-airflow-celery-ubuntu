dfd14edbba17357212bfa35d8c8a6e5bf4b288f9
felrobotics <lposadaa@eafit.edu.co>
2022-06-09 01:13:46 +0000

    airflow schedulerer added

---
+++ setup/setup_services.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# set -euo pipefail
+
+##-------------  HELPERS   --------------##
+# colors
+nc=$(echo -en '\033[0m')
+red=$(echo -en '\033[00;31m')
+green=$(echo -en '\033[00;32m')
+yellow=$(echo -en '\033[00;33m')
+
+# pretty printing
+function msg {
+  # echo -e "${green}$(date): ${yellow}$1${nc}"
+  echo -e "${green}[$(date +"%Y/%m/%d - %H:%M:%S")] ${yellow}$1${nc}" # better date format
+}
+
+
+##-------------  START OF THE SCRIPT  --------------##
+
+msg "Creating airflow scheduler service symbolic link"
+sudo ln -s /home/airflow/deploy-airflow-celery-ubuntu/services/airflow_scheduler.service /etc/systemd/system/airflow_scheduler.service
+
+msg "Restart service"
+sudo systemctl daemon-reload
+sudo systemctl restart airflow_scheduler.service
+
+msg "Service status"
+systemctl status airflow_scheduler.service
